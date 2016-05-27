ef79cfdae7787e7039e0b26d6e2c5a838f85cc3f
Emanuelle <emanuelle.portillo@hotmail.com>
2016-05-26 20:37:33 -0600

    Cambios para incluir el Stemmer

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 /**/
+var r = new FileReader();
 function handleFileSelect(evt) {
     evt.stopPropagation();
     evt.preventDefault();
@@ -13,18 +14,17 @@ function handleFileSelect(evt) {
             output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
                 f.size, ' bytes, last modified: ',
                 f.lastModifiedDate.toLocaleDateString(), '</li>');
-            var r = new FileReader();
             r.onload = function(e) { 
                 var contents = e.target.result;
-                $('#texto').html($('#texto').html()+contents+"\n");
+                $('#texto').val($('#texto').val()+contents+"\n");
             }
             r.readAsText(f);  
             ban = true;
         }                                   
     }
-    if(ban){
+    //if(ban){
         document.getElementById('list').innerHTML += output.join('');   
-    }   
+    //}   
 }
 
 function handleDragOver(evt) {
@@ -32,8 +32,37 @@ function handleDragOver(evt) {
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
-jQuery
 // Setup the dnd listeners.
 var dropZone = document.getElementById('drop_zone');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
+var ban_stemmer = false;
+jQuery(document).ready(function(e){
+    function make_stemmer(){
+        if(ban_stemmer == false){
+            var lines = jQuery('#texto').val().split("\n"); ///([\wáéíóú]+) ?(\n\r?)?/gi
+            jQuery('#texto').val("");
+            //var new_lines = []
+            for (var i = 0; i < lines.length; i++) {                
+                var line = lines[i];
+                words = line.split(/([\wáéíóú]+) \s/i);
+                var new_words = [];
+                for(j = 0; j < words.length; j++){
+                    var word = words[j];
+                    new_words.push(Stemmer.stemm(word));
+                }
+                jQuery('#texto').val(jQuery('#texto').val()+new_words.join(" ") + "\n");
+            }
+            ban_stemmer = true;
+        }
+    }
+    jQuery('.btn>input').click(function(evt){
+        if(jQuery('#texto').val() != ''){
+            if(this.id == 'stemmer'){
+                make_stemmer();
+            }
+            jQuery(this).prop('checked',true);
+            jQuery(this).addClass("active");
+        }
+    });
+})
